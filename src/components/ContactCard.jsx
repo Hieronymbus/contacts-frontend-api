7d9cfe4809600050ab45759471bd9203435d23e1
Dave-ctr <davidjamieson1985@gmail.com>
2024-09-18 07:38:05 +1200

    ContactCard Component Updated

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const ContactCard = ({contact}) => {
+const ContactCard = ({ contact, handleCardClose }) => {
   return (
-    <div
-      className='absolute z-10 bg-white h-3/4 w-5/6'
-    >
-      {  contact.id}
-        {contact.name}
-        {contact.email}
-        {contact.phone}
-        {contact.image}
-        {contact.dob}
+    <div className="p-4 absolute z-10 bg-white w-11/12 flex flex-col justify-center items-center gap-4 text-4xl border-2 border-stone-950 rounded-xl">
+    <div className="min-w-80 min-h-80 flex justify-center items-center">
+      <img src={ contact.image} alt="Profile Pic" className="rounded-full " onClick={handleCardClose}></img>
+      </div>
+      <p className="font-bold">{contact.name}</p>
+      <p>Email Address</p>
+      <p>{contact.email}</p>
+      <p>Phone Number</p>
+      <p>{contact.phone}</p>
+      <p>Birthday</p>
+      <p>{contact.dob}</p>
     </div>
-  )
-}
+  );
+};
 
-export default ContactCard
+export default ContactCard;

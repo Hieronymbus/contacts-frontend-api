7ee5124367c1d4c351c09d381edd4a2408fb8ff9
Hieronymbus <birdsallalexander1990@gmail.comm>
2024-09-17 15:10:53 +1200

    added header and contactCard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Header from './components/Header';
+import ContactsList from './components/ContactsList';
 import { useState } from 'react'
 import './index.css';
 
@@ -6,6 +8,8 @@ function App() {
 
   return (
     <>
+<Header />
+<ContactsList />
      
     </>
   )
---
+++ src/components/CreateContactForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateContactForm = () => {
+  return (
+    <div>CreateContactForm</div>
+  )
+}
+
+export default CreateContactForm

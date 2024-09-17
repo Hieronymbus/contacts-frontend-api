674781514a3f888a9ae7903c4c9a560147eb906b
Hieronymbus <birdsallalexander1990@gmail.comm>
2024-09-17 14:36:49 +1200

    created components for contactList, CreateContact,SearchBar

---
+++ src/components/CreateContact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from 'react'
+
+function CreateContact() {
+  return (
+    <div>CreateContact</div>
+  )
+}
+
+export default CreateContact
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from 'react'
+
+function SearchBar() {
+  return (
+    <div>SearchBar</div>
+  )
+}
+
+export default SearchBar
